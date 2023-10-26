4fe9624dbbf210aa6ddc14982632862fef3216fa
StaHstaH <stanislaw.sipowicz@backdoorstudios.pl>
2023-10-26 19:54:06 +0200

    Added life parameters functionality
    Added decreasing life parameters to the UI

--- js/modules/tamagotchi.js
+++ js/modules/tamagotchi.js
@@ -4,15 +4,54 @@ export default class Tamagotchi {
     this.hunger = { value: 10, importance: 3 };
     this.energy = { value: 10, importance: 2 };
     this.fun = { value: 10, importance: 4 };
+    this.updateCounter = 0;
     console.log("Tamagotchi initialized");
   }
 
-  displayHealth = (elementSelector) => {
-    const displayElement = document.querySelector(elementSelector);
-    displayElement.innerText = this.energy.value;
+  displayParameter = (param) => {
+    const displayElement = param.displayElement;
+    displayElement.innerText = param.value;
+
+    if (param.value > 9) {
+      displayElement.classList.add("high_value");
+    } else {
+      displayElement.classList.remove("high_value");
+    }
+  };
+
+  mountParameter = (param, elementSelector) => {
+    param.displayElement = document.querySelector(elementSelector);
+    this.displayParameter(param);
   };
 
   mount = ({ healthElement, hungerElement, energyElement, funElement }) => {
-    this.displayHealth(healthElement);
+    this.mountParameter(this.health, healthElement);
+    this.mountParameter(this.hunger, hungerElement);
+    this.mountParameter(this.energy, energyElement);
+    this.mountParameter(this.fun, funElement);
+  };
+
+  decrementParameter = (param) => {
+    if (param.value > 0) {
+      param.value--;
+      this.displayParameter(param);
+    }
+  };
+
+  update = () => {
+    this.updateCounter++;
+    this.decrementParameter(this.hunger);
+    this.decrementParameter(this.fun);
+
+    if (this.hunger.value <= 0 || this.energy.value <= 0) {
+      this.decrementParameter(this.health);
+    }
+
+    if (this.updateCounter % 2 === 0) {
+      this.decrementParameter(this.energy);
+      if (this.fun.value <= 0) {
+        this.decrementParameter(this.energy);
+      }
+    }
   };
 }
